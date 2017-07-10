cb1ac5b75c82f4051fd791d71c0aa24ab8512705
vagrant <vagrant@intranet>
2017-07-10 20:45:38 +0000

    Adding custom JWT Service Provider Register

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Intranet\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Intranet\Jwt\Manager;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('tymon.jwt.manager', function ($app) {
+            $instance = new Manager(
+                $app['tymon.jwt.provider.jwt'],
+                $app['tymon.jwt.blacklist'],
+                $app['tymon.jwt.payload.factory']
+            );
+
+            return $instance->setBlacklistEnabled((bool) config('jwt.blacklist_enabled'))
+                ->setPersistentClaims(config('jwt.persistent_claims'));
+        });
     }
 }
